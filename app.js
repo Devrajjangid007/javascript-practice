8a20013b9b347ee3dc9fa2502f36e8c357884317
Devraj jangid <72245676+Devrajjangid007@users.noreply.github.com>
2025-10-29 23:39:26 +0530

    Update app.js

--- app.js
+++ app.js
@@ -1,2 +1,48 @@
 (12 < 13 ? console.log('ok ji chal gyr'): console.log('hum nhi clenge'))
-console.log('or sab thhik')
+
+
+let a = 12;
+
+a += 12
+console.log(a)
+
+a -= 12
+console.log(a)
+
+a *= 2;
+console.log(a)
+
+a /= 2
+console.log(a)
+
+a %= 2 
+console.log(a)
+
+
+
+a = 12 
+console.log(a)
+
+
+
+if(12 == "12"){
+    console.log('true')
+}
+else{
+    console.log('false')
+}
+
+
+if(12 === "12"){
+    console.log('true ==')
+}
+else{
+    console.log('false === ')
+}
+
+if(12 !== "12"){
+    console.log('true')
+}
+else{
+    console.log('false')
+}
