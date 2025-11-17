1dc22f6ee4735172c42c37d834aa35e1f1e9bd66
Devraj jangid <72245676+Devrajjangid007@users.noreply.github.com>
2025-11-17 23:11:41 +0530

    nop

--- app.js
+++ app.js
@@ -439,5 +439,20 @@ min.addEventListener('click', function(){
     }
    
     })
+
+
+
+
+
+    let box = document.querySelector('.box')
+    let change = document.querySelector('.change')
     
 
+    change.addEventListener('click', function(){
+
+        let c1 = Math.floor(Math.random()*256);
+        let c2 = Math.floor(Math.random()*256);
+        let c3 = Math.floor(Math.random()*256);
+        box.style.backgroundColor = `rgb(${c1} , ${c2} , ${c3} )`
+
+    })
